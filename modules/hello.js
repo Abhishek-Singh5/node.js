c082f6b982269c55d94a6fa6684a380d23dacf35
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-07-12 09:52:57 +0530

    Add files via upload

---
+++ modules/hello.js
@@ -0,0 +1,7 @@
+import { sub, add, module, multi } from "./math.js";
+
+
+console.log("Add Value is ", add(2, 5));
+console.log("Subtract Value is ", sub(2, 5));
+console.log("Multiply Value is ", multi(2, 5));
+console.log("Module Value is ", module(2, 4));
