2f006ca0257b1d1cf6c2a6bb16d6777ae7b26ec9
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-07-23 20:33:36 +0530

    Add files via upload

---
+++ Authentication/controllers/user.js
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+import User from '../models/user.js';
+import { setUser } from '../service/auth.js'
+
+
+
+export async function handleUserSignup(req, res) {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.render("signup", { error: "User already exists" });
+    }
+
+    await User.create({ name, email, password });
+    return res.redirect("/login");
+}
+
+export async function handleUserLogin(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+
+    if(!user) {
+        return res.render("login", {
+            error: "Invalid Username or Password",
+            
+        })
+
+    }
+
+        const sessionId = uuidv4();
+        setUser(sessionId, user);
+        res.cookie('uid', sessionId);
+        return res.redirect("/");
+}
